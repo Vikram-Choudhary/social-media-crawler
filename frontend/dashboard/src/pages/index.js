359f39fa4f2939e842774065719b294129a44de0
Vikram Choudhary <vikramus4@gmail.com>
2024-12-11 14:50:21 -0500

    frontend changes added

--- frontend/dashboard/src/pages/index.js
+++ frontend/dashboard/src/pages/index.js
@@ -1,28 +1,33 @@
 import { useState, useEffect } from "react";
-import SentimentChart from "./SentimentChart";
+import SentimentChart4Chan from "./SentimentChart4Chan";
+import SentimentChartReddit from "./SentimentChartReddit";
+import SubRedditCounts from "./SubRedditCounts";
 
 export default function Home() {
   const [connectionStatus, setConnectionStatus] = useState(0); // 0: Loading, 1: Connected, -1: Failed
 
-  useEffect(() => {
-    const controller = new AbortController();
-    const timeoutId = setTimeout(() => {
-      controller.abort();
-      setConnectionStatus(-1);
-    }, 5000);
-
+  useEffect(() => {  
     const checkAPIConnection = async () => {
+      const controller = new AbortController();
+      const timeoutId = setTimeout(() => {
+        controller.abort(); // Abort the fetch request after 5 seconds
+      }, 10000);
+
       try {
         console.log("Request: /testconnection");
-        const response = await fetch("http://127.0.0.1:8000/testconnection", {
-          signal: controller.signal,
-        });
+        const response = await fetch("http://127.0.0.1:8000/testconnection", { signal: controller.signal });
+        
+        clearTimeout(timeoutId);
+
         if (response.ok) {
           await response.json();
-          clearTimeout(timeoutId);
           setConnectionStatus(1);
+        } else {
+          setConnectionStatus(-1);
         }
       } catch (error) {
+        clearTimeout(timeoutId);
+
         if (error.name === "AbortError") {
           console.error("Connection request timed out.");
         } else {
@@ -33,17 +38,20 @@ export default function Home() {
     };
 
     checkAPIConnection();
-
-    return () => {
-      clearTimeout(timeoutId);
-      controller.abort();
-    };
   }, []);
 
   const renderContent = () => {
     switch (connectionStatus) {
       case 0:
-        return <div className="error-msg"> <div className="app-loader"><div className="loader"></div>Loading...</div>Checking API connection...</div>;
+        return (
+          <div className="error-msg">
+            <div className="app-loader">
+              <div className="loader"></div>
+              Loading...
+            </div>
+            Checking API connection...
+          </div>
+        );
       case -1:
         return (
           <div className="error-msg">
@@ -52,9 +60,13 @@ export default function Home() {
         );
       case 1:
         return (
-          <div style={{ width: "600px", margin: "0 auto" }}>
-            <h1>4chan Job Market Sentiment Analysis</h1>
-            <SentimentChart />
+          <div style={{padding:30}}>
+            <h1 style={{padding:30, justifySelf:'center'}}>Job Market Sentiment Analysis</h1>
+            <div className="chart-container">
+              <div className='items' style={{ width: "600px", margin: "0 auto" }}> <SentimentChart4Chan /></div>
+              <div className='items' style={{ width: "600px", margin: "0 auto" }}><SentimentChartReddit /></div>
+              <div className='items' style={{ width: "600px", margin: "0 auto" }}><SubRedditCounts/></div>
+            </div>
           </div>
         );
       default:
